3d41c862e2e67b014d37a7ba8aaac3abb849da66
Rusty <legaleagle94262@gmail.com>
2026-07-06 18:52:34 -0500

    Slingshot adjustments

--- Content/Items/Weapons/WoodSlingshot.cs
+++ Content/Items/Weapons/WoodSlingshot.cs
@@ -5,6 +5,7 @@ using LargsMod.Common;
 using LargsMod.Content.DamageClasses;
 using LargsMod.Content.Items;
 using LargsMod.Content.Items.Ammo;
+using Microsoft.Xna.Framework;
 
 namespace LargsMod.Content.Items.Weapons
 {
@@ -12,9 +13,14 @@ namespace LargsMod.Content.Items.Weapons
     {
         public override LargsWeaponType WeaponType => LargsWeaponType.Static;
 
+        public override Vector2? HoldoutOffset()
+        {
+          return new Vector2(-8f, 0f);
+        }
+
         public override void SetDefaults()
         {
-            Item.damage = 4;
+            Item.damage = 2;
             Item.DamageType = ModContent.GetInstance<Largs>();
 
             Item.width = 32;
@@ -28,7 +34,7 @@ namespace LargsMod.Content.Items.Weapons
             Item.knockBack = 2f;
 
             Item.UseSound = SoundID.Item5;
-            Item.autoReuse = true;
+            Item.autoReuse = false;
 
             Item.shoot = ProjectileID.PurificationPowder;
             Item.useAmmo = ModContent.ItemType<PelletAmmo>();
